46441c3aca284a9240a69d09618fab9140e7934c
187397511 <187397511@ignou>
2024-06-12 15:57:43 +0530

    pages and navigation

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact_us', views.contact_us, name='contact us'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'pages/home.html')
 
+def about(request):
+    return render(request, 'pages/about.html')
+def services(request):
+    return render(request, 'pages/services.html')
+def contact_us(request):
+    return render(request, 'pages/contact_us.html')
+
 # Create your views here.
