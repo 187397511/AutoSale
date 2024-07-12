e170ed44cba4344788038050c5ba39e7a38580a8
187397511 <187397511@ignou>
2024-07-12 21:21:42 +0530

    updated settings.py

--- AutoSale/settings.py
+++ AutoSale/settings.py
@@ -155,7 +155,7 @@ MESSAGE_TAGS = {
 SITE_ID = 3
 
 #email
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.protonmail.ch'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
